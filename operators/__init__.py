cef11ecd0a79bfc5acc6bae1cd03c2c870f82c33
ShioN <shion@arch4e.com>
2023-02-21 16:55:10 +0900

    feat: add some classes to change brush blending mode in texture paint mode

--- operators/__init__.py
+++ operators/__init__.py
@@ -1,6 +1,8 @@
 if "bpy" not in locals():
     import bpy
+    from . import tp_bbmode
     from . import wp_bbmode
 else:
     import importlib
+    importlib.reload(tp_bbmode)
     importlib.reload(wp_bbmode)
